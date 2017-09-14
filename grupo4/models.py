a183869d441dd3cd023bdbb4941b67b4484d82d4
kevinmaidana <maidanakevin35@gmail.com>
2017-09-14 01:25:37 -0300

    todo listo

---
+++ grupo4/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Agencia(models.Model):
+    nombre = models.CharField(max_length=20)
+    def __str__(self):
+            return self.title
+
+class Persona(models.Model):
+    dni = models.CharField(max_length=8)
+    tipoDeTrabajoQuePuedeRealizar = models.ForeignKey('Rubro')
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    fechaDeNacimiento = models.DateField()
+    desocupado = models.BooleanField()
+    def __str__(self):
+            return self.title
+
+class Empleo(models.Model):
+    persona = models.ForeignKey('Persona')
+    oferta = models.ForeignKey('Oferta')
+    empresa = models.ForeignKey('Empresa')
+    inicioContrato = models.DateField()
+    finContrato = models.DateField()
+    def __str__(self):
+            return self.title
+
+class Rubro(models.Model):
+    tipoDeTrabajo = models.CharField(max_length=20)
+    def __str__(self):
+            return self.title
+
+class Oferta(models.Model):
+    empresa = models.ForeignKey('Empresa')
+    necesidad = models.ForeignKey('Rubro')
+    activa = models.BooleanField()
+    inicioContrato = models.DateField()
+    finContrato = models.DateField()
+    def __str__(self):
+            return self.title
+
+class Empresa(models.Model):
+    cuit = models.CharField(max_length=20)
+    razonSocial = models.CharField(max_length=30)
+    rubro = models.ForeignKey('Rubro')
+    activa = models.BooleanField()
+    def __str__(self):
+            return self.title
