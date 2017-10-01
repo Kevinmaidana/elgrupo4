62b888ef1bb8b6c7cff8abdb3df48d193d0f9a64
Alan Rodas Bonjour <alanrodas@gmail.com>
2017-10-01 16:32:01 -0300

    Eliminado entorno, cache y agregado registro

--- grupo4/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-        url(r'^$', views.grupo4),
-        url(r'^persona/', views.personas_list),
-        
-    ]
